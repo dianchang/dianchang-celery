8c55aa8c377885f0c964ef9fff983ad6ca3d2c88
hustlzp <hustlzp@qq.com>
2015-07-02 12:36:25 +0800

    For testing.

--- dc/models/_base.py
+++ dc/models/_base.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 mode = os.environ.get('MODE')
+print(mode)
 if mode == 'PRODUCTION':
     app.config.update(
         SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:sBLkMvu1PlRuZEAEerqa@119.254.102.136:3306/dianchang",
