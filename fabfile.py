e96fa45a0312480b9d3e3b2f9131850929daa2e8
hustlzp <hustlzp@qq.com>
2015-07-02 14:57:50 +0800

    Add fabfile.

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from fabric.api import local, run, env, cd, prefix, shell_env
+
+host_string = "root@119.254.101.73"
+
+
+def deploy():
+    """部署"""
+    env.host_string = host_string
+    with cd('/var/www/dianchang-celery'):
+        with shell_env(MODE='PRODUCTION'):
+            run('git reset --hard HEAD')
+            run('git pull')
+            with prefix('source venv/bin/activate'):
+                run('pip install -r requirements.txt')
+            run('supervisorctl restart celerybeat')
+            run('supervisorctl restart celery')
+
+
+def restart():
+    """重启"""
+    env.host_string = host_string
+    run('supervisorctl restart celerybeat')
+    run('supervisorctl restart celery')
