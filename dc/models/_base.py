2d4bfa28053298edd103fc8c037d9975684f2d9c
hustlzp <hustlzp@qq.com>
2015-07-02 12:46:27 +0800

    Rm testing codes.

--- dc/models/_base.py
+++ dc/models/_base.py
@@ -4,7 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 mode = os.environ.get('MODE')
-print(mode)
 if mode == 'PRODUCTION':
     app.config.update(
         SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:sBLkMvu1PlRuZEAEerqa@119.254.102.136:3306/dianchang",
