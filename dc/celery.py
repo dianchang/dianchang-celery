29f5ce5d45510ada199d826004fc43d601bf3f53
hustlzp <hustlzp@qq.com>
2015-07-15 20:20:45 +0800

    Add Sentry support.

--- dc/celery.py
+++ dc/celery.py
@@ -1,7 +1,11 @@
+# coding: utf-8
 from __future__ import absolute_import
+import logging
 from datetime import timedelta
 from celery import Celery
 from celery.schedules import crontab
+from raven import Client
+from raven.contrib.celery import register_signal, register_logger_signal
 
 app = Celery('dc',
              broker='redis://localhost:6379/0',
@@ -31,6 +35,12 @@ app.conf.update(
         }
     }
 )
+
+# 注册 Sentry
+client = Client(dsn="http://98ab6589bb944ca381a69d32e5e5e5d2:c4edeac245204b81bf843ef145dc04f3@119.254.101.73:9000/4")
+register_logger_signal(client)
+register_signal(client)
+register_logger_signal(client, loglevel=logging.INFO)
 
 if __name__ == '__main__':
     app.start()
